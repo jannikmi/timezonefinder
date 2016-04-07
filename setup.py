1b7f2c017e4031ce76a565f808a8eaa6cf3be344
Adam Chainz <me@adamj.eu>
2016-04-07 13:57:47 +0100

    Check in setup.cfg and setup.py
    These files should be checked in to git so people can reproduce your package.
    
    I've taken the latest versions from pip and checked them in, with the update that the README is read from `README.md` directly rather than pasted into a string.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='timezonefinder',
+    version='1.003',
+    packages=['timezonefinder'],
+    package_data={'timezonefinder': ['timezone_data.bin']},
+    description='Python library to look up timezone from lat / long offline. Improved version of "pytzwhere".',
+    author='J. Michelfeit',
+    author_email='python@michelfe.it',
+    license='MIT licence',
+    url='https://github.com/MrMinimal64/timezonefinder',  # use the URL to the github repo
+    download_url='https://github.com/MrMinimal64/timezonefinder/tarball/1.0',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Localization',
+    ],
+    long_description=readme
+)
