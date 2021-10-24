ef135bc2611ff65e871d7c31fda4d69cb3791638
MrMinimal64 <github@michelfe.it>
2021-10-24 02:39:16 +0200

    poetry dependency management

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     },
     keywords="timezone coordinates latitude longitude location pytzwhere tzwhere",
     install_requires=["numpy>=1.16"],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     # TODO http://peak.telecommunity.com/DevCenter/setuptools#setting-the-zip-safe-flag
     #  safe if the project uses pkg_resources for all its data file access
     # http://peak.telecommunity.com/DevCenter/setuptools#accessing-data-files-at-runtime
