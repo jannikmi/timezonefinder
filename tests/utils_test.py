139b7a094f9a2f5d6310834bdb3f94c7a121b39c
Jannik Kissinger <github@michelfe.it>
2024-09-18 15:41:04 +0200

    separate clang util module

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -9,8 +9,9 @@ from tests.auxiliaries import (
     get_rnd_poly_int,
     get_rnd_query_pt,
 )
-from timezonefinder import utils
+from timezonefinder import utils, utils_clang
 from timezonefinder.configs import DTYPE_FORMAT_H_NUMPY, INT2COORD_FACTOR
+from timezonefinder.utils_clang import clang_extension_loaded
 
 POINT_IN_POLYGON_TESTCASES = [
     # (polygon, list of test points, expected results)
@@ -175,11 +176,17 @@ def test_convert2ints():
     np.testing.assert_almost_equal(ints_converted, ints_true)
 
 
+def test_clang_extension_loaded():
+    assert clang_extension_loaded, "the clang extension not loaded, "
+
+
+# TODO parametrize the test cases
+# TODO test equal results of both implementation
 @pytest.mark.parametrize(
     "inside_poly_func",
     [
         utils.pt_in_poly_python,
-        utils.pt_in_poly_clang,
+        utils_clang.pt_in_poly_clang,
     ],
 )
 @pytest.mark.parametrize(
@@ -221,6 +228,7 @@ def test_inside_polygon(inside_poly_func: Callable, test_case: Tuple):
     assert nr_mistakes == 0
 
 
+# TODO @pytest.mark.parametrize(
 def test_rectify_coords():
     # within bounds -> no exception
     utils.validate_coordinates(lng=180.0, lat=90.0)
