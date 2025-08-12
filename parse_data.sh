9b7c1052cea7abbf960773f39e4d1c1cbff77c9f
Jannik Kissinger <github@michelfe.it>
2025-08-12 10:16:18 +0200

    Update parse_data.sh

--- parse_data.sh
+++ parse_data.sh
@@ -5,7 +5,7 @@ ARCHIVE_NAME=data_downloaded.zip
 ZIP_ARCHIVE_PATH=./$WORKING_FOLDER_NAME/$ARCHIVE_NAME
 JSON_PREFIX=combined
 JSON_SUFFIX=.json
-# DESTINATION_PATH=./timezonefinder
+DESTINATION_PATH=./timezonefinder
 URL_PREFIX=https://github.com/evansiroky/timezone-boundary-builder/releases/latest/download/timezones
 URL_SUFFIX=.geojson.zip
 
@@ -60,8 +60,7 @@ fi
 echo "START PARSING..."
 SCRIPT_PATH=./scripts/file_converter.py
 echo "calling $SCRIPT_PATH:"
-#  -out "$DESTINATION_PATH"
-python "$SCRIPT_PATH" -inp "$JSON_PATH"
+python "$SCRIPT_PATH" -inp "$JSON_PATH"  -out "$DESTINATION_PATH"
 
 echo "runnings tests..."
 if ! tox; then
