df2b6c6eed584f7c7adbc79c9e3983684ce1988f
Jannik Kissinger <github@michelfe.it>
2025-08-12 09:52:00 +0200

    Update get_geometry.py

--- examples/get_geometry.py
+++ examples/get_geometry.py
@@ -3,9 +3,11 @@ from timezonefinder import TimezoneFinder
 tf = TimezoneFinder()
 
 # Example using timezone name
-geometry_name = tf.get_geometry(tz_name="Africa/Addis_Ababa", coords_as_pairs=True)
-print(f"Geometry for Africa/Addis_Ababa:\n{geometry_name}")
+timezone_name = "Europe/Paris"
+geometry_by_name = tf.get_geometry(tz_name=timezone_name, coords_as_pairs=True)
+print(f"Geometry for timezone '{timezone_name}':\n{geometry_by_name}")
 
-# Example using timezone ID (optional)
-geometry_id = tf.get_geometry(tz_id=400, use_id=True)
-print(f"Geometry for timezone ID 400:\n{geometry_id}")
+# Example using timezone ID
+timezone_id = 12
+geometry_by_id = tf.get_geometry(tz_id=timezone_id, use_id=True)
+print(f"Geometry for timezone ID {timezone_id}:\n{geometry_by_id}")
